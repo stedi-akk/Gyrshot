a5588c21eb206a99aec2c3ffc6e6695341c115c8
Dima Stepanchenko <stepanchenkodmitriy@gmail.com>
2016-12-03 12:46:58 +0100

    hide navbar on api 19+; added retrolambda

--- app/src/main/java/com/stedi/gyrshot/MainActivity.java
+++ app/src/main/java/com/stedi/gyrshot/MainActivity.java
@@ -1,6 +1,21 @@
 package com.stedi.gyrshot;
 
 import android.app.Activity;
+import android.os.Build;
+import android.view.View;
 
 public class MainActivity extends Activity {
+    @Override
+    protected void onResume() {
+        super.onResume();
+        hideNavbar();
+    }
+
+    private void hideNavbar() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    | View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        }
+    }
 }
